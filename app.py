735181b80fc8027912cb6c812da4518581cc70ed
Peter Schutt <peter.github@proton.me>
2024-04-29 07:31:52 +1000

    refactor: use sqladmin-litestar-plugin

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
 from litestar import Litestar
-from sqladmin import Admin, ModelView
+from sqladmin import ModelView
+from sqladmin_litestar_plugin import SQLAdminPlugin
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import declarative_base
 
-from db import User, engine
+
+engine = create_async_engine("sqlite+aiosqlite:///example.db")
+Base = declarative_base()
 
 
-admin = Admin(engine)
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
 
 
 class UserAdmin(ModelView, model=User):
     column_list = [User.id, User.name]
 
 
-admin.add_view(UserAdmin)
+async def on_startup() -> None:
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)  # Create tables
 
-app = Litestar(plugins=[admin], debug=True)
+
+admin = SQLAdminPlugin(views=[UserAdmin], engine=engine)
+app = Litestar(plugins=[admin], on_startup=[on_startup])
